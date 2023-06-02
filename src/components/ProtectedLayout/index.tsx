7c7712e573545785170cd7332a8544a0149c1514
Paulinha <paulafabianasoares@gmail.com>
2023-06-02 15:24:19 -0300

    delete do context/Auth/getKey.tsx; modificação nos componentes ProtectedLayout e Login para o redirecionamento do usuario se ele já estiver logado

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -1,12 +1,24 @@
-import { useAuth } from '../../context/Auth/useAuth';
-import Login from '../../pages/Login';
+import Login from "../../pages/Login";
+import {
+  getUserLocalStorage,
+  getKeyLocalStorage,
+  getSubscriptionLocalStorage,
+} from "../utils/utilAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
-    const auth = useAuth();
-    if (!auth.user) {
-        return <Login />
-    }
-    return children;
-}
+  const user = getUserLocalStorage();
+  const key = getKeyLocalStorage();
+  const subscription = getSubscriptionLocalStorage();
+  const location = useLocation();
+  const navigate = useNavigate();
+  if (!user && !key && !subscription) {
+    return <Login />;
+  }
+  if (location.pathname === "/") {
+    navigate("/home");
+  }
+  return children;
+};
 
-export default ProtectedLayout
+export default ProtectedLayout;
